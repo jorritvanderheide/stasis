3e10253e07c3a5b5e4529ce98f16bb07d70c53f1
Jorrit van der Heide <bw20@noreply.codeberg.org>
2025-11-12 15:59:31 +0100

    feat: add home manager configuration

--- modules/home/stasis.nix
+++ modules/home/stasis.nix
@@ -1,18 +1,146 @@
 {
   osConfig,
-  pkgs,
   lib,
   ...
 }:
 
 let
-  inherit (lib) mkIf;
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkOption
+    types
+    ;
+
   cfg = osConfig.services.stasis;
 in
 {
+  options.services.stasis = {
+    enable = mkEnableOption "Stasis configuration";
+
+    config = mkOption {
+      type = types.nullOr types.lines;
+      default = null;
+      description = ''
+        The literal contents of the Stasis configuration file.
+
+        If set, Home Manager will write this text to
+        `~/.config/stasis/config.rune`.
+      '';
+      example = ''
+        @author "Dustin Pilgrim"
+        @description "Stasis configuration file"
+
+        # Default timeout for apps (in seconds)
+        app_default_timeout 300
+
+        #
+        # Stasis configuration
+        #
+        stasis:
+          pre_suspend_command "hyprlock"
+          monitor_media true
+          ignore_remote_media true
+          respect_idle_inhibitors true
+          #lid_close_action "lock-screen" (lock-screen | suspend | custom | ignore)
+          #lid_open_action "wake" (wake | custom | ignore)
+          # debounce: default is 3s; can be customized if needed
+          # debounce-seconds 4
+
+          inhibit_apps [
+            "vlc"
+            "Spotify"
+            "mpv"
+            r".*\.exe"
+            r"steam_app_.*"
+            r"firefox.*"
+          ]
+
+          # desktop-only idle actions (applies to all devices)
+          lock_screen:
+            timeout 300
+            command "swaylock"
+            resume-command "notify-send 'Welcome Back $env.USER!'"
+          end
+
+          dpms:
+            timeout 60
+            command "niri msg action power-off-monitors"
+            resume-command "niri msg action power-on-monitors"
+          end
+
+          suspend:
+            timeout 1800
+            command "systemctl suspend"
+            resume-command None
+          end
+
+          # laptop-only AC actions
+          on_ac:
+
+            # Step 1: Adjust brightness (instant)
+            # define it first so it does not retrigger later
+            custom-brightness-instant:
+              timeout 0
+              command "brightnessctl set 100%"
+            end
+
+            lock_screen:
+              timeout 300
+              command "swaylock"
+            end
+
+            brightness:
+              timeout 10
+              command "brightnessctl set 30%"
+            end
+
+            dpms:
+              timeout 60
+              command "niri msg action power-off-monitors"
+            end
+
+            suspend:
+              timeout 300
+              command "systemctl suspend"
+            end
+          end
+
+          # Laptop-only battery actions
+          on_battery:
+            custom-brightness-instant:
+              timeout 0
+              command "brightnessctl set 100%"
+            end
+
+            lock_screen:
+              timeout 120
+              command "swaylock"
+            end
+
+            brightness:
+              timeout 5
+              command "brightnessctl set 30%"
+            end
+
+            dpms:
+              timeout 5
+              command "niri msg action power-off-monitors"
+            end
+
+            suspend:
+              timeout 120
+              command "systemctl suspend"
+            end
+          end
+        end
+      '';
+    };
+  };
+
   config = mkIf cfg.enable {
-    xdg.configFile."stasis/config.rune" = {
-      source = (pkgs.formats.rune { }).generate "stasis/config.rune" cfg.settings;
+    xdg.configFile."stasis/config.rune".text = mkIf (cfg.config != null) {
+      text = cfg.config;
     };
   };
 }
